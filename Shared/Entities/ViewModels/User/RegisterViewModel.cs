965e8b807ac0b2fd2caecb6b895cf673611b0e33
Armend Ukehaxhaj <armendd.u@hotmail.com>
2018-12-28 12:31:09 +0100

    Added RegisterViewModel for user registration

--- Shared/Entities/ViewModels/User/RegisterViewModel.cs
+++ Shared/Entities/ViewModels/User/RegisterViewModel.cs
@@ -1,7 +1,36 @@
-﻿namespace Entities.ViewModels.User
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.ViewModels.User
 {
     public class RegisterViewModel
     {
-        
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        public string ReTypePassword { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+
+        public string Phone { get; set; }
+
+        [Required]
+        public DateTime Birthdate { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
     }
 }
