058a817c91274a13a9e709140ac0803cb4f06d42
Armend Ukehaxhaj <armendd.u@hotmail.com>
2019-03-01 18:59:24 +0100

    Changed dashboardviewmodel

--- Shared/Entities/ViewModels/DashboardViewModel.cs
+++ Shared/Entities/ViewModels/DashboardViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Entities.ViewModels
 {
     public class DashboardViewModel
     {
+        public IEnumerable<Models.Game> GamesList { get; set; }
         public string MinutesTillNextGame { get; set; }
         public int RegisteredUsers { get; set; }
         public decimal CurrentPotOfGame { get; set; }
